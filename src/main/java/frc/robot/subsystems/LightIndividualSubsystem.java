93e63cb483c9fa71ba4740ef97a9ebe0d755cd33
awall <rewfus@gmail.com>
2024-02-07 22:32:08 -0600

    idea for changing brightness while preserving color

--- src/main/java/frc/robot/subsystems/LightIndividualSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightIndividualSubsystem.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems;
 
+import java.awt.*;
 import java.util.Optional;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LightIndividualSubsystem extends SubsystemBase {
@@ -13,12 +15,10 @@ public class LightIndividualSubsystem extends SubsystemBase {
     private AddressableLED addressableLED;
     private AddressableLEDBuffer addressableLEDBuffer;
     private int hue;
-    private int saturation;
-    private int lightness;
+    private int saturation = 255;
+    private int brightness = 128;
 
     public LightIndividualSubsystem() {
-        lightness = 0;
-        saturation = 255;
         addressableLED = new AddressableLED(1);
         addressableLEDBuffer = new AddressableLEDBuffer(60);
         addressableLED.setLength(addressableLEDBuffer.getLength());
@@ -43,26 +43,34 @@ public class LightIndividualSubsystem extends SubsystemBase {
         }
     }
 
-    public void blank() {
+    private void changeBrightness(int newBrightness) {
+        brightness = newBrightness;
         for (int i = 0; i < addressableLEDBuffer.getLength(); i++) {
-            addressableLEDBuffer.setHSV(i, 0, 0, 0);
+            Color8Bit color = addressableLEDBuffer.getLED8Bit(i);
+            float[] hsvVals = Color.RGBtoHSB(color.red, color.green, color.blue, null);
+            addressableLEDBuffer.setHSV(i, (int)(hsvVals[0] * 180.0), (int)(hsvVals[1] * 255.0), brightness);
         }
     }
 
+    public void blank() {
+        changeBrightness(0);
+    }
+
     public void lightsOn(){
-        addressableLEDBuffer.setHSV(addressableLEDBuffer.getLength(), 0, 0, 155);
+        changeBrightness(128);
     }
 
     public void progressBarColorrrr(double progress, int hue, int saturation) {
         int length = (int) (addressableLEDBuffer.getLength() * progress);
         for (int i = 0; i < length; i++) {
-            addressableLEDBuffer.setHSV(i, hue, saturation, 155);
+            addressableLEDBuffer.setHSV(i, hue, saturation, brightness);
         }
     }
 
     public void setColor(int hue, int saturation) {
-
-        addressableLEDBuffer.setHSV(addressableLEDBuffer.getLength(), hue, saturation, 0);
+        for (int i = 0; i < addressableLEDBuffer.getLength(); i++) {
+            addressableLEDBuffer.setHSV(i, hue, saturation, brightness);
+        }
     }
 
     public void rainbow() {
@@ -72,7 +80,7 @@ public class LightIndividualSubsystem extends SubsystemBase {
             // shape is a circle so only one value needs to precess
             final int hues = (int) (hue + (i * 180 / addressableLEDBuffer.getLength())) % 180;
             // Set the value
-            addressableLEDBuffer.setHSV(i, hues, 255, 128);
+            addressableLEDBuffer.setHSV(i, hues, 255, brightness);
         }
         // Increase by to make the rainbow "move"
         hue += 1;
