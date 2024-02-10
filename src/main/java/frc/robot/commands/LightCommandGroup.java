5c2ed036eefe5edd97fdf76ccd2936a68c1ea694
Dylan Carter <dylan.carter@lhssc.org>
2024-02-09 20:42:58 -0600

    started to create a command group and need to switch to dev

--- src/main/java/frc/robot/commands/LightCommandGroup.java
+++ src/main/java/frc/robot/commands/LightCommandGroup.java
@@ -1,5 +1,15 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.LightIndividualSubsystem;
+
 public class LightCommandGroup {
     
+    public static Command blankOn(LightIndividualSubsystem lightIndividualSubsystem){
+        return new ParallelCommandGroup (
+                    new LightBlankCommand(lightIndividualSubsystem).withTimeout(3),
+                    new LightsOnCommand(lightIndividualSubsystem).withTimeout(3));
+
+    }
 }
