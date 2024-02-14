6e648ebcfeb21755e36df3c5154bcfe11c012f8a
khaim <khai.wall@stem2u.org>
2024-02-13 20:49:15 -0800

    elevator motors are moving in the right direction

---
+++ src/main/java/frc/robot/commands/ElevatorUpCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorUpCommand extends Command {
+
+      public ElevatorSubsystem elevatorSubsystem;
+
+    public ElevatorUpCommand(ElevatorSubsystem elevatorSubsystem) {
+        this.elevatorSubsystem = elevatorSubsystem;
+        
+        addRequirements(elevatorSubsystem);
+
+    }
+
+    @Override
+    public void execute() {
+      
+        elevatorSubsystem.elevatorUp();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+    
+    
+
+}
+
+    
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -34,26 +34,24 @@ public class ElevatorSubsystem extends SubsystemBase {
     private CANSparkMax elevatorMotor2;
     private RelativeEncoder elevatorEncoder;
     private SparkPIDController elevatorPID;
+    private GenericEntry elevatorPositioGenericEntry;
 
     ElevatorAutoLogged elevatorAutoLogged;
-    private double setPoint;
     private final double tolerance = 0.1;
+    private double setPoint = 0;
 
     // private SparkAnalogSensor sparkAnalogSensor;
     // GenericEntry digiput;
     // GenericEntry digiputLimit;
 
-
-
     private SparkLimitSwitch m_reverseLimit;
-
 
     public ElevatorSubsystem() {
         elevatorAutoLogged = new ElevatorAutoLogged();
 
-        elevatorMotor1 = SparkFactory.createCANSparkMax(Constants.CANIDConstants.elevatorMotor1, false);
-        elevatorMotor2 = SparkFactory.createCANSparkMax(Constants.CANIDConstants.elevatorMotor2, false);
-
+        elevatorMotor1 = SparkFactory.createCANSparkMax(Constants.CANIDConstants.elevatorMotor1, true);
+        elevatorMotor2 = SparkFactory.createCANSparkMax(Constants.CANIDConstants.elevatorMotor2, true);
+        elevatorMotor1.setInverted(true);
         elevatorPID = elevatorMotor1.getPIDController();
         elevatorEncoder = elevatorMotor1.getEncoder();
         elevatorMotor1.enableSoftLimit(SoftLimitDirection.kForward, true);
@@ -64,32 +62,38 @@ public class ElevatorSubsystem extends SubsystemBase {
         elevatorMotor1.setSoftLimit(SoftLimitDirection.kReverse, ElevatorSetpoints.ZERO.getValue());
         elevatorMotor1.enableVoltageCompensation(Constants.voltageCompensation);
         elevatorMotor2.enableVoltageCompensation(Constants.voltageCompensation);
+        elevatorPositioGenericEntry = Shuffleboard.getTab("Arm Angle").add("Elevator position", 0).getEntry();
+   
 
-        elevatorMotor2.follow(elevatorMotor1, true);
+
+        elevatorMotor2.follow(elevatorMotor1, false);
 
         elevatorEncoder.setPosition(0);
 
-        elevatorPID.setP(0.1);
+        elevatorPID.setP(0);
         elevatorPID.setI(0);
-        elevatorPID.setD(0.1);
+        elevatorPID.setD(0);
         elevatorPID.setFF(0);
 
-        
-        // elevatorEncoder.setPositionConversionFactor(1 / Constants.ArmAngleSubsystemConstants.armGearRatio);
-        
-        /*digitalInput = new DigitalInput(0);
-        digiput = Shuffleboard
-        .getTab("MagenetSensor")
-        .add("MagnetSensor", 1)
-        .withWidget(BuiltInWidgets.kGraph)
-        .getEntry();*/
-        
-        m_reverseLimit = elevatorMotor1.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
-        m_reverseLimit.enableLimitSwitch(true);
-        //digiputLimit = Shuffleboard.getTab("MagnetSensor").add("MagnetSwitchStatus", false).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
-        
+        // elevatorEncoder.setPositionConversionFactor(1 /
+        // Constants.ArmAngleSubsystemConstants.armGearRatio);
         elevatorMotor1.burnFlash();
         elevatorMotor2.burnFlash();
+
+        /*
+         * digitalInput = new DigitalInput(0);
+         * digiput = Shuffleboard
+         * .getTab("MagenetSensor")
+         * .add("MagnetSensor", 1)
+         * .withWidget(BuiltInWidgets.kGraph)
+         * .getEntry();
+         */
+
+        m_reverseLimit = elevatorMotor1.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
+        m_reverseLimit.enableLimitSwitch(true);
+        // digiputLimit = Shuffleboard.getTab("MagnetSensor").add("MagnetSwitchStatus",
+        // false).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
+
     }
 
     public void setElevatorPose(ElevatorSetpoints elevatorSetpoints) {
@@ -104,19 +108,37 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     }
 
-public void updateInputs(Elevator elevator) {
-    elevator.position=elevatorEncoder.getPosition();
-    elevator.setpoint=setPoint;
-    Logger.processInputs("George", elevatorAutoLogged);
-}
+    public void elevatorUp() {
+
+        if (setPoint < ElevatorSetpoints.ONEHUNDRED.getValue() - 0.1) {
+
+            setPoint += 0.1;
+        }
+    }
+
+    public void elevatorDown() {
+
+        if (setPoint > ElevatorSetpoints.ZERO.getValue() - 0.1) {
+
+            setPoint -= 0.1;
+        }
+    }
+
+    public void updateInputs(Elevator elevator) {
+        elevator.position = elevatorEncoder.getPosition();
+        elevator.setpoint = setPoint;
+        Logger.processInputs("George", elevatorAutoLogged);
+    }
 
     @Override
     public void periodic() {
         updateInputs(elevatorAutoLogged);
-
-    elevatorPID.setReference(setPoint, ControlType.kPosition);
-    /*digiput.setDouble(digitalInput.get()?1:0);
-    digiputLimit.setBoolean(m_reverseLimit.isPressed());*/
+        elevatorPositioGenericEntry.setDouble(setPoint);
+        elevatorPID.setReference(setPoint, ControlType.kPosition);
+        /*
+         * digiput.setDouble(digitalInput.get()?1:0);
+         * digiputLimit.setBoolean(m_reverseLimit.isPressed());
+         */
 
     }
 
