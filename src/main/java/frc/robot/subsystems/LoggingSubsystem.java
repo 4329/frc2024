a65e71da71bff77bec1d0eac30cb84f509ec7357
Cameron Bell <cameron.bell@lhssc.org>
2024-02-22 21:01:43 -0600

    Began Subsystem logging

---
+++ src/main/java/frc/robot/subsystems/LoggingSubsystem.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LoggingSubsystem extends SubsystemBase{
+
+    private LoggedSubsystem[] subsystems;
+
+    public LoggingSubsystem(LoggedSubsystem... subsystems) {
+        this.subsystems = subsystems;
+    }
+
+    @Override
+    public void periodic() {
+
+        for (LoggedSubsystem subsystemLog : subsystems) {
+            
+            Logger.processInputs(subsystemLog.getClass().getName(), subsystemLog.log());
+            
+        }
+
+    }
+    
+}
