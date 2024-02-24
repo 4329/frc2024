1407514af361999a041f515457fe17cb78fb8972
MBorchert <alphaom34@gmail.com>
2024-02-23 19:18:37 -0600

    Added a timer so autologging actually works.

--- src/main/java/frc/robot/subsystems/LoggingSubsystem.java
+++ src/main/java/frc/robot/subsystems/LoggingSubsystem.java
@@ -2,23 +2,31 @@ package frc.robot.subsystems;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LoggingSubsystem extends SubsystemBase {
 
     private LoggedSubsystem[] subsystems;
+    public Timer timer;
 
     public LoggingSubsystem(LoggedSubsystem... subsystems) {
         this.subsystems = subsystems;
+        timer = new Timer();
+        timer.start();
     }
 
     @Override
     public void periodic() {
 
-        for (LoggedSubsystem subsystemLog : subsystems) {
-            
-            Logger.processInputs(subsystemLog.getClass().getName(), subsystemLog.log());   
+        if (timer.hasElapsed(1)) {
+
+            for (LoggedSubsystem subsystemLog : subsystems) {
+
+                Logger.processInputs(subsystemLog.getClass().getName(), subsystemLog.log());
+            }
+            timer.restart();
         }
     }
-    
+
 }
