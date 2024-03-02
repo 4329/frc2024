1663a7db091258eece02b74d9133a7bff1ca47a0
MBorchert <95452187+MBorchert@users.noreply.github.com>
2024-03-02 09:42:18 -0600

    log the commands (#53)
    Co-authored-by: awall <rewfus@gmail.com>

---
+++ src/main/java/frc/robot/utilities/CommandLoginator.java
@@ -0,0 +1,16 @@
+package frc.robot.utilities;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class CommandLoginator {
+    public CommandLoginator() {
+        init();
+    }
+    private void init() {
+
+        CommandScheduler.getInstance().onCommandInitialize(new CommandConsumingLogger("initialize()"));
+        CommandScheduler.getInstance().onCommandExecute(new CommandConsumingLogger("execute()"));
+        CommandScheduler.getInstance().onCommandFinish(new CommandConsumingLogger("end(true)"));
+        CommandScheduler.getInstance().onCommandInterrupt(new CommandConsumingLogger("end(false)"));
+    }
+}
