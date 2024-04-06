45829aa01f0b6d3d290a89ab670a8274b77b135a
alphaom33 <alphaom34@gmail.com>
2024-04-05 19:26:47 -0500

    stupid

--- src/main/java/frc/robot/subsystems/lightSubsystem/LightIO.java
+++ src/main/java/frc/robot/subsystems/lightSubsystem/LightIO.java
@@ -6,18 +6,14 @@ public interface LightIO {
   // AddressableLEDBuffer getBuffer();
 
   default void setHSV(int index, int hue, int saturation, int lightness) {}
-  ;
 
   default int getLength() {
     return 0;
   }
-  ;
 
   default Color8Bit getLED8Bit(int index) {
     return new Color8Bit();
   }
-  ;
 
   default void periodic() {}
-  ;
 }
