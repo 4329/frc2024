b3d8722def96fc8c26242fc786a5c2d377aea568
khaim <khai.wall@stem2u.org>
2024-04-10 20:20:48 -0700

    no understage autos

--- src/main/java/frc/robot/commands/AutoShootCommand.java
+++ src/main/java/frc/robot/commands/AutoShootCommand.java
@@ -1,9 +1,14 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.armCommands.ArmCommand;
+import frc.robot.commands.elevatorCommands.ElevatorCommand;
+// import frc.robot.commands.elevatorCommands.ElevatorShootCommand;
+import frc.robot.commands.elevatorCommands.ElevatorShootCommandIndefinite;
+import frc.robot.commands.shootCommands.ShooterAimCommand;
 import frc.robot.commands.shootCommands.ShooterAimCommandIndefinite;
 import frc.robot.commands.shootCommands.ShooterShotCommand;
 import frc.robot.commands.shootCommands.ShotRevCommand;
@@ -13,6 +18,7 @@ import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.utilities.ArmAngle;
+import frc.robot.utilities.ElevatorSetpoints;
 
 public class AutoShootCommand extends SequentialCommandGroup {
   public AutoShootCommand(
@@ -25,15 +31,16 @@ public class AutoShootCommand extends SequentialCommandGroup {
 
     super(
         new ParallelRaceGroup(
-            new ShotRevCommand(shootSubsystem, visionSubsystem).withTimeout(3),
-            //  new CenterOnTargetCommandIndefinite(visionSubsystem, drivetrain,
-            // AprilTagUtil.getAprilTagSpeakerIDAprilTagIDSpeaker(), commandXboxController),
-            new ShooterAimCommandIndefinite(visionSubsystem, armAngleSubsystem, elevatorSubsystem)),
+            new ParallelCommandGroup(
+                new ShooterAimCommand(visionSubsystem, armAngleSubsystem),
+                // new ElevatorShootCommand(elevatorSubsystem, visionSubsystem)),
+                new ShotRevCommand(shootSubsystem, visionSubsystem))),
         new ParallelRaceGroup(
-            new ShooterShotCommand(shootSubsystem, indexSubsystem, visionSubsystem),
-            //   new CenterOnTargetCommandIndefinite(visionSubsystem, drivetrain,
-            // AprilTagUtil.getAprilTagSpeakerIDAprilTagIDSpeaker(), commandXboxController),
-            new ShooterAimCommandIndefinite(visionSubsystem, armAngleSubsystem, elevatorSubsystem)),
-        new ArmCommand(armAngleSubsystem, ArmAngle.INTAKE));
+            new ShooterShotCommand(shootSubsystem, indexSubsystem, visionSubsystem).withTimeout(2),
+            new ShooterAimCommandIndefinite(visionSubsystem, armAngleSubsystem, elevatorSubsystem),
+            new ElevatorShootCommandIndefinite(elevatorSubsystem, visionSubsystem)),
+        new ParallelCommandGroup(
+            new ArmCommand(armAngleSubsystem, ArmAngle.INTAKE),
+            new ElevatorCommand(elevatorSubsystem, ElevatorSetpoints.ZERO)));
   }
 }
