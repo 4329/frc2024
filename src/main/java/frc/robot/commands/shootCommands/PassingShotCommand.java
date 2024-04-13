32bf22cf31d26e26995d8518bc9897aef7dbdd3f
khaim <khai.wall@stem2u.org>
2024-04-13 13:58:47 -0700

    finished passing notes

--- src/main/java/frc/robot/commands/shootCommands/PassingShotCommand.java
+++ src/main/java/frc/robot/commands/shootCommands/PassingShotCommand.java
@@ -6,50 +6,53 @@ import frc.robot.subsystems.ArmAngleSubsystem;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
 import frc.robot.utilities.ArmAngle;
+import frc.robot.utilities.ShotRpms;
 
 public class PassingShotCommand extends Command {
 
-    private ShootSubsystem shootSubsystem;
-    private ArmAngleSubsystem armAngleSubsystem;
-    private IndexSubsystem indexSubsystem;
-    private Timer timer = new Timer();
-    private boolean shot = false;
+  private ShootSubsystem shootSubsystem;
+  private ArmAngleSubsystem armAngleSubsystem;
+  private IndexSubsystem indexSubsystem;
+  private Timer timer = new Timer();
+  private boolean shot = false;
 
-    public PassingShotCommand(ShootSubsystem shootSubsystem, ArmAngleSubsystem armAngleSubsystem, IndexSubsystem indexSubsystem) {
-        this.shootSubsystem = shootSubsystem;
-        this.armAngleSubsystem = armAngleSubsystem;
-        this.indexSubsystem = indexSubsystem;
-        addRequirements(shootSubsystem, armAngleSubsystem, indexSubsystem);
-    }
-
-    @Override
-    public void initialize() {
-        shot = false;
-        timer.reset();
-        armAngleSubsystem.setArmAngle(ArmAngle.PASS);
-        shootSubsystem.setRPM(3500);
-    }
-
-    @Override
-    public void execute() {
-        if (shootSubsystem.aboveSetpoint() && !shot){
-            timer.start();
-            indexSubsystem.inShoot();
-            shot = true;
-        }
-    }
+  public PassingShotCommand(
+      ShootSubsystem shootSubsystem,
+      ArmAngleSubsystem armAngleSubsystem,
+      IndexSubsystem indexSubsystem) {
+    this.shootSubsystem = shootSubsystem;
+    this.armAngleSubsystem = armAngleSubsystem;
+    this.indexSubsystem = indexSubsystem;
+    addRequirements(shootSubsystem, armAngleSubsystem, indexSubsystem);
+  }
 
   @Override
-    public boolean isFinished() {
-        return timer.hasElapsed(0.6);    
-    }
+  public void initialize() {
+    shot = false;
+    timer.reset();
+    armAngleSubsystem.setArmAngle(ArmAngle.PASS);
+    shootSubsystem.setRPM(ShotRpms.PASS.getValue());
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        shootSubsystem.stop();
-        indexSubsystem.stop();
-        armAngleSubsystem.setArmAngle(ArmAngle.INTAKE);
-        timer.stop();
+  @Override
+  public void execute() {
+    if (shootSubsystem.aboveSetpoint() && !shot) {
+      timer.start();
+      indexSubsystem.inShoot();
+      shot = true;
     }
-  
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(0.6);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    shootSubsystem.stop();
+    indexSubsystem.stop();
+    armAngleSubsystem.setArmAngle(ArmAngle.INTAKE);
+    timer.stop();
+  }
 }
