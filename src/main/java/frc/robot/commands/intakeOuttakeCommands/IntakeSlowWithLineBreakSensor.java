33a55484b1bf9d9d66f5ed73657f37815bda8e84
khaiwall <98569493+khaiwall@users.noreply.github.com>
2024-04-13 14:01:24 -0500

    Worldautos (#85)
    * making some new autos
    
    * more autos
    
    * no understage autos
    
    * made more autos
    
    * another auto change
    
    * autostuff
    
    * made some autos at Rr

---
+++ src/main/java/frc/robot/commands/intakeOuttakeCommands/IntakeSlowWithLineBreakSensor.java
@@ -0,0 +1,31 @@
+package frc.robot.commands.intakeOuttakeCommands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.armCommands.ArmCommand;
+import frc.robot.commands.indexCommands.IndexReverseForShotCommand;
+import frc.robot.commands.indexCommands.IndexSensorCommand;
+import frc.robot.subsystems.ArmAngleSubsystem;
+import frc.robot.subsystems.IndexSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LineBreakSensorSubsystem;
+import frc.robot.subsystems.ShootSubsystem;
+import frc.robot.utilities.ArmAngle;
+import frc.robot.utilities.UnInstantCommand;
+
+public class IntakeSlowWithLineBreakSensor extends SequentialCommandGroup {
+  public IntakeSlowWithLineBreakSensor(
+      IntakeSubsystem intakeSubsystem,
+      IndexSubsystem indexSubsystem,
+      LineBreakSensorSubsystem lineBreakSensorSubsystem,
+      ShootSubsystem shootSubsystem,
+      ArmAngleSubsystem armAngleSubsystem) {
+    super(
+        new ParallelCommandGroup(
+            new ArmCommand(armAngleSubsystem, ArmAngle.INTAKE),
+            new IntakeSensorCommand(intakeSubsystem, lineBreakSensorSubsystem),
+            new IndexSensorCommand(lineBreakSensorSubsystem, indexSubsystem)),
+        new IndexReverseForShotCommand(lineBreakSensorSubsystem, indexSubsystem),
+        new UnInstantCommand(() -> shootSubsystem.changeSetpoint(-100)));
+  }
+}
