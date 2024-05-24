2f5b6a1a8284a8e9bb7f27918afa3a5bc3f209e3
alphaom33 <matthew.borchert@lhssc.org>
2024-05-23 20:06:44 -0500

    protected

--- src/main/java/frc/robot/subsystems/LightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightSubsystem.java
@@ -1,9 +1,15 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.littletonrobotics.junction.Logger;
 
 public class LightSubsystem extends SubsystemBase {
 
@@ -11,57 +17,91 @@ public class LightSubsystem extends SubsystemBase {
   LEDPattern currentPattern;
 
   public static class State {
-    public static boolean shooting;
-    public static boolean climbing;
-    public static boolean outdexing;
-    public static boolean intaking;
-    public static boolean hasNote;
-    public static boolean sourcing;
-    public static boolean inRange;
+    private static boolean shooting;
+    private static boolean climbing;
+    private static boolean outdexing;
+    private static boolean intaking;
+    private static boolean hasNote;
+    private static boolean sourcing;
+    private static boolean inRange;
 
-    public static boolean isShooting() {
+    protected static boolean isShooting() {
       return shooting;
     }
     public static void setShooting(boolean shooting) {
       State.shooting = shooting;
     }
-    public static boolean isClimbing() {
+    protected static boolean isClimbing() {
       return climbing;
     }
     public static void setClimbing(boolean climbing) {
       State.climbing = climbing;
     }
-    public static boolean isOutdexing() {
+    protected static boolean isOutdexing() {
       return outdexing;
     }
     public static void setOutdexing(boolean outdexing) {
       State.outdexing = outdexing;
     }
-    public static boolean isIntaking() {
+    protected static boolean isIntaking() {
       return intaking;
     }
     public static void setIntaking(boolean intaking) {
       State.intaking = intaking;
     }
-    public static boolean isHasNote() {
+    protected static boolean isHasNote() {
       return hasNote;
     }
     public static void setHasNote(boolean hasNote) {
       State.hasNote = hasNote;
     }
-    public static boolean isSourcing() {
+    protected static boolean isSourcing() {
       return sourcing;
     }
     public static void setSourcing(boolean sourcing) {
       State.sourcing = sourcing;
     }
-    public static boolean isInRange() {
+    protected static boolean isInRange() {
       return inRange;
     }
     public static void setInRange(boolean inRange) {
       State.inRange = inRange;
     }
   }
+
+  private class Node {
+    public LEDPattern pattern;
+    public List<Edge> edges;
+
+    public Node(LEDPattern pattern, List<Edge> edges) {
+      this.pattern = pattern;
+      this.edges = edges;
+    }
+
+    public void addEdge(Edge toAdd) {
+      edges.add(toAdd);
+    }
+
+    public void evalEdges() {
+      edges.forEach((x) -> {
+        if (x.whenGo.get()) {
+          current = x.next;
+          setLEDPattern(current.pattern);
+        }
+      });
+    }
+  }
+  private class Edge {
+    public Node next;
+    public Supplier<Boolean> whenGo;
+
+    public Edge(Node next, Supplier<Boolean> whenGo) {
+      this.next = next;
+      this.whenGo = whenGo;
+    }
+  }
+  private Node current;
+  private Timer timer = new Timer();
 
   public LightSubsystem() {
     int count = 0;
@@ -78,6 +118,19 @@ public class LightSubsystem extends SubsystemBase {
 
     if (count == 2) Logger.recordOutput("Lights", "no :(");
     else Logger.recordOutput("Lights", "Yes!");
+  
+    setUpGraph();
+    timer.start();
+  }
+
+  private void setUpGraph() {
+    Node head = new Node(LEDPattern.NOTHING, new ArrayList<>());
+    current = head;
+    setLEDPattern(head.pattern);
+
+    head.addEdge(new Edge(new Node(LEDPattern.ORANGE, new ArrayList<>()), () -> {
+      return timer.hasElapsed(10);
+    }));
   }
 
   public enum LEDPattern {
@@ -111,6 +164,6 @@ public class LightSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    
+    current.evalEdges();
   }
 }
